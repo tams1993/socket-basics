0c19a3e32c531fe64530dcaae4d2afc05d907402
tams1993 <tams1993@gmail.com>
2016-11-15 16:18:12 +0700

    Add fronte-end form

--- server.js
+++ server.js
@@ -8,11 +8,26 @@ var http = require('http').Server(app);
 var socket = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
-socket.on('connection', function () {
+socket.on('connection', function (socket) {
     console.log('User connected via socket.io! ');
+
+    socket.on('message', function (message) {
+
+        console.log('Message received:' + message.text);
+
+        // Send to everybody but sender
+        socket.broadcast.emit('message', message);
+    });
+
+    socket.emit('message', {
+
+        text: 'Welcome to the chat application!'
+
+    });
+
 });
 http.listen(PORT, function () {
 
     console.log('Server Started');
-    
+
 });
