e691e11e0ad6317fd02578c17e9f2aa0475c1056
tams1993 <tams1993@gmail.com>
2016-11-16 14:15:45 +0700

    Add styles for chat app

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,8 @@ socket.on('connect', function () {
 socket.on('message', function (message) {
 
     var momentTimestamp = moment.utc(message.timestamp);
-    var $message = jQuery('.messages')
+    var $messages = jQuery('.messages')
+    var $message = jQuery('<li class="list-group-item"></li>')
     console.log('New message:' );
     console.log(message.text);
 
@@ -32,7 +33,7 @@ socket.on('message', function (message) {
     // })
     $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format("h:mm:ss A") + ': </strong></p>');
     $message.append('<p>' + message.text + '</p>');
-
+    $messages.append($message);
 });
 
 // Handles submitting of new message
